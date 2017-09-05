c3920e425a5f42ae3acf02054ea627777a21a065
谢恩泽 <Johnny_ez@163.com>
2017-09-05 15:46:04 +0800

    Update KuduCRUD.scala

--- src/main/scala/com/spark/test/KuduCRUD.scala
+++ src/main/scala/com/spark/test/KuduCRUD.scala
@@ -1,8 +1,11 @@
-package com.spark.test
+package com.xieenze.kudutst
 
 /**
-  * Created by xieenze on 2017/9/1.
+  * Created by xieenze on 2017/9/5.
   */
+
+import org.apache.kudu.Type
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
@@ -11,118 +14,59 @@ import org.apache.spark.sql.functions._
 import org.apache.kudu.spark.kudu._
 import org.apache.kudu.client._
 import org.apache.log4j.{Level, Logger}
+
 /**
   * Spark on Kudu 的案例
   *
-  * 使用spark对于kudu表 展示 insert, update and scan 操作.
+  * 使用spark对于kudu表 展示 insert, update and scan 操作.并且可以修改表结构，如增加一列等
+  * 主要使用spark集成的kuduContext和kudu自带的kuduClient两个客户端
   *
   */
-
-object test {
+object kuduCRUD_demo {
   Logger.getLogger("org").setLevel(Level.ERROR)
   case class Customer(name:String, age:Int, city:String)
+  val spark = SparkSession.builder
+    .appName("TestKuduDF")
+    .master("local[*]")
+    .config("spark.sql.warehouse.dir", "spark-warehouse")
+    .getOrCreate()
+  val sc = spark.sparkContext
+  val sqlContext = spark.sqlContext
+  import spark.implicits._
+
+  val kuduMasters = "10.64.217.195:7051"
+  val kuduContext = new KuduContext(kuduMasters,sc)
+  val kuduTableName = "tj_student"
+  val kuduOptions = Map("kudu.master" -> "10.64.217.195:7051",
+    "kudu.table" -> kuduTableName,
+    "kudu.operation.timeout.ms" -> "10000")
+
+  val customers = Array(
+    Customer("jane", 30, "new york"),
+    Customer("jordan1111111111111111", 18, "toronto"),
+    Customer("enzo2222222222222222", 43, "oakland"),
+    Customer("laura33333333333333333333", 27, "vancouver"))
+  val customersRDD = sc.parallelize(customers)
+  val customersDF = customersRDD.toDF()
+
+  val client = new KuduClient.KuduClientBuilder(kuduMasters).build()
+
+
   def main(args: Array[String]): Unit = {
-
-    // Setup Spark configuration and related contexts
-    val sparkConf = new SparkConf().
-      setMaster("local[*]").
-      setAppName("Spark Kudu Up and Running Example").
-      set("spark.driver.allowMultipleContexts","true")
-    // Create a Spark and SQL context
-    val sc = new SparkContext(sparkConf)
-    val sqlContext = new SQLContext(sc)
-
-
-
-    // Comma-separated list of Kudu masters with port numbers
-    val kuduMasters = "10.64.217.195:7051"
-    // Create an instance of a KuduContext
-    val kuduContext = new KuduContext(kuduMasters,sc)
-    // This allows us to implicitly convert RDD to DataFrame
-    import sqlContext.implicits._
-
-    // Specify a table name
-    var kuduTableName = "spark_kudu_tbl"
-
-    // Define Kudu options used by various operations
-    val kuduOptions: Map[String, String] = Map(
-      "kudu.table"  -> kuduTableName,
-      "kudu.master" -> kuduMasters)
-
-
-    // Check if the table exists, and drop it if it does
-    if (kuduContext.tableExists(kuduTableName)) {
-      kuduContext.deleteTable(kuduTableName)
-    }
-
-    ///-------------------------------------------------------------------------------------
-    /// 创建表
-    // 1. Give your table a name
-    kuduTableName = "spark_kudu_tbl"
-
-    // 2. Define a schema
-    val kuduTableSchema = StructType(
-      //        column name   type       nullable
-      StructField("name", StringType , false) ::
-        StructField("age" , IntegerType, true ) ::
-        StructField("city", StringType , true ) :: Nil)
-
-    // 3. Define the primary key
-    val kuduPrimaryKey = Seq("name")
-
-    // 4. Specify any further options
-    val kuduTableOptions = new CreateTableOptions()
-    kuduTableOptions.
-      setRangePartitionColumns(List("name").asJava).
-      setNumReplicas(1)
-
-    // 5. Call create table API
-    kuduContext.createTable(
-      // Table name, schema, primary key and options
-      kuduTableName, kuduTableSchema, kuduPrimaryKey, kuduTableOptions)
-
-    /// WRITING TO TABLE - KUDU CONTEXT
-    // Define a list of customers based on the case class already defined above
-    val customers = Array(
-      Customer("jane", 30, "new york"),
-      Customer("jordan", 18, "toronto"))
-
-    // Create RDD out of the customers Array
-    val customersRDD = sc.parallelize(customers)
-
-    // Now, using reflection, this RDD can easily be converted to a DataFrame
-    // Ensure to do the 
-    //     import sqlContext.implicits._
-    // above to have the toDF() function available to you
-    val customersDF = customersRDD.toDF()
-
-
-    // 5. Read data from Kudu table
-    sqlContext.read.options(kuduOptions).kudu.show
-    ///-------------------------------------------------------------------------------------
-
-
-    ///-------------------------------------------------------------------------------------
-    /// 插入数据
-    // 1. 指定kudu表
-    kuduTableName = "spark_kudu_tbl"
-
-    // 2. 把dataFrame格式的数据插入kudu表
-    kuduContext.insertRows(customersDF, kuduTableName)
-
-    // 3. 从kudu中读取记录并显示
-    sqlContext.read.options(kuduOptions).kudu.show
-    ///-------------------------------------------------------------------------------------
-
+    //deleteColumns()
+    //addColumms()
+    //renameColumms()
   }
 
-  def createTable(kuduTableName:String,kuduContext: KuduContext) ={
+  //创建表
+  def createTable(kuduTableName:String) ={
     //1 定义表结构
-    val kuduTableSchema = StructType(
-      StructField("name",StringType,false)::
-      StructField("age" , IntegerType, true ) ::
-      StructField("city", StringType , true ) :: Nil
-    )
+    //两种方式 1 通过现成的datafram创建表，2 自己定义表结构
+    val kuduTableSchema = customersDF.schema
+    /*val kuduTableSchema = StructType(
+          StructField(name="name",dataType=StringType,nullable=false)::
+            StructField("age" , IntegerType, true ) ::
+            StructField("city", StringType , true ) :: Nil)*/
     //2 定义主键
     val kuduTablePrimaryKey = Seq("name")
     //3 定义一些其他的配置信息
@@ -130,43 +74,62 @@ object test {
     //表的复制和分区选项
     val kuduTableOptions = new CreateTableOptions()
     kuduTableOptions.
-      //暂时不明白干嘛的
-      setRangePartitionColumns(List("name").asJava).
-      //设置分区 数据分到几台机器上
-      setNumReplicas(2)
+      setRangePartitionColumns(List("name").asJava).//暂时不明白干嘛的
+      setNumReplicas(1).// 设置副本数量
+      addHashPartitions(List("name").asJava,20)//设置分区 数据分到几台机器上
     //4 创建表
     kuduContext.createTable(kuduTableName,kuduTableSchema,kuduTablePrimaryKey,kuduTableOptions)
   }
-  def deleteTable(kuduTableName:String,kuduContext: KuduContext): Unit ={
+  //删除表
+  def deleteTable(kuduTableName:String): Unit ={
     kuduContext.deleteTable(kuduTableName)
   }
-  def isTableExists(kuduTableName:String,kuduContext: KuduContext): Unit ={
+  //判断表是否存在
+  def isTableExists(kuduTableName:String): Unit ={
     val flag = kuduContext.tableExists(kuduTableName)
     flag
   }
-  def insertRowsToTableUseKuduAPI(kuduTableName:String,kuduContext:KuduContext,dataDF:DataFrame): Unit ={
+  //插入数据
+  def insertRowsToTableUseKuduAPI(kuduTableName:String,dataDF:DataFrame): Unit ={
     kuduContext.insertRows(dataDF,kuduTableName)
   }
+  //增加一列，通过kuduClient操作，详情参考https://kudu.apache.org/apidocs/org/apache/kudu/client/KuduClient.html
+  def addColumms(): Unit ={
+    val ato = new AlterTableOptions
+    ato.addColumn("height",Type.STRING,"170")
+    client.alterTable(kuduTableName,ato)
+  }
+  //删除一列
+  def deleteColumns(): Unit ={
+    val ato = new AlterTableOptions
+    ato.dropColumn("height")
+    client.alterTable(kuduTableName,ato)
+  }
+  //重命名一列
+  def renameColumms(): Unit ={
+    val ato = new AlterTableOptions
+    ato.renameColumn("height","heightttt")
+    client.alterTable(kuduTableName,ato)
+  }
+  //使用sparkSQL读取数据，返回DataFrame
+  def readDataFromTableUseDataFrame() ={
+    val customerReadDF = spark.read.options(kuduOptions).kudu
+    customerReadDF
+  }
+
+  /*
+  剩下的看这2位老哥的代码吧，懒得写了，没啥难度
+  https://github.com/mladkov/spark-kudu-up-and-running/blob/master/src/main/scala/com/cloudera/examples/spark/kudu/SparkKuduUpAndRunning.scala
+  https://github.com/jimmy-src/kudu-learning/blob/d5330ca3d725c3a47997e0d1adc321cc7afec65c/src/main/KuduSparkIntergration/KuduSpark.scala
+   */
   def deleteRowsFromTable(): Unit ={
     //TODO
-    //https://github.com/mladkov/spark-kudu-up-and-running/blob/master/src/main/scala/com/cloudera/examples/spark/kudu/SparkKuduUpAndRunning.scala#L48
   }
   def upsertToTable(): Unit ={
     //TODO
   }
   def updateToTable(): Unit ={
     //TODO
-  }
-  //使用kudu api读取数据，返回kuduRDD
-  def readDataFromTableUseKuduAPI(sc:SparkContext,kuduTableName:String,kuduTableProjColumns:Seq[String],kuduContext:KuduContext) ={
-    val custRDD = kuduContext.kuduRDD(sc, kuduTableName, kuduTableProjColumns)
-    val a = custRDD.collect()
-    custRDD
-  }
-  //使用sparkSQL读取数据，返回DataFrame
-  def readDataFromTableUseDataFrame(sqlContext: SQLContext,kuduOptions:Map[String,String]) ={
-    val customerReadDF = sqlContext.read.options(kuduOptions).kudu
-    customerReadDF
   }
   def insertToTableUseDataFrameAPI(): Unit ={
     //TODO
@@ -178,4 +141,5 @@ object test {
   def selectUsePredicatePushdown(): Unit ={
     //TODO
   }
+
 }
